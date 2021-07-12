7b7e81f838b8b5ddf4a1cffb3da6f8d8b1b8c824
egorkaras1997@gmail.com <egorkaras1997@gmail.com>
2021-07-12 15:28:28 +0300

    Add some features

--- JsonExample/JsonExample/Program.cs
+++ JsonExample/JsonExample/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Text.Json;
 
 namespace JsonExample
 {
     class Program
     {
+        class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person jarry = new Person{Name = "Jarry", Age = 23};
+            Person tom = new Person { Name = "Tom", Age = 31 };
+            string json = JsonSerializer.Serialize<Person>(jarry);
+            string tomJson = JsonSerializer.Serialize<Person>(tom);
+            Console.WriteLine(json);
+            Console.WriteLine(tom);
+            Person restoredPerson = JsonSerializer.Deserialize<Person>(json);
+            Console.WriteLine(restoredPerson.Name);
         }
     }
 }
