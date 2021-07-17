4a3d8e21cf79d391ade25a69124cabf987cd4ffb
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-07-17 22:13:07 +0300

    Use SinglePrinsiple Patter in wrong and right way.

---
+++ SOLIDPrinciple/SingleResponsibility/RightMonitorIpAddressOfPC.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SingleResponsibility
+{
+    public static class RightMonitorIpAddressOfPC
+    {
+        public static List<string> GetIpAddress()
+        {
+            var hostName = Dns.GetHostName();
+            var ipHost = Dns.GetHostEntry(hostName);
+
+            return ipHost.AddressList.Select(item => item.ToString()).ToList();
+        }
+    }
+
+    public class PrintData
+    {
+        private static List<string> _data4Print;
+
+        public PrintData(List<string> data4Print)
+        {
+            _data4Print = data4Print;
+        }
+        public void PrintIpAddrToConsole()
+        {
+            foreach (var item in _data4Print)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public async Task PrintIpAddrToTextFile()
+        {
+            using (StreamWriter sw = new StreamWriter("IpAddress.txt"))
+            {
+                foreach (var item in _data4Print)
+                {
+                    await sw.WriteLineAsync(item);
+                }
+            }
+        }
+    }
+}
---
+++ SOLIDPrinciple/SingleResponsibility/WrongMonitorIpAddressOfPC.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    public static class WrongMonitorIpAddressOfPC
+    {
+        private static List<string> _ipAddres = new List<string>();
+
+        public static void GetIpAddress()
+        {
+            var hostName = Dns.GetHostName();
+            var ipHost = Dns.GetHostEntry(hostName);
+
+            foreach (var item in ipHost.AddressList)
+            {
+                _ipAddres.Add(item.ToString());
+            }
+        }
+
+        //PrintBlock
+        public static void PrintIpAddrToConsole()
+        {
+            foreach (var item in _ipAddres)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static async Task PrintIpAddrToTextFile()
+        {
+            using (StreamWriter sw = new StreamWriter("IpAddress.txt"))
+            {
+                foreach (var item in _ipAddres)
+                {
+                    await sw.WriteLineAsync(item);
+                }
+            }
+        }
+    }
+}
