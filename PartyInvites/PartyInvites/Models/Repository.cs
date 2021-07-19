68055fa01948edcba20131470f0a81edfb9e932c
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-07-19 17:43:38 +0300

    Party Invites from Frimen ASP.NET MVC

---
+++ PartyInvites/PartyInvites/Models/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponce> responses = new List<GuestResponce>();
+
+        public static IEnumerable<GuestResponce> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+
+        public static void AddResponse(GuestResponce response)
+        {
+            responses.Add(response);
+        }
+    }
+}
